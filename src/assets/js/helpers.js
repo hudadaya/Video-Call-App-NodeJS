384242451cfaf600966ae8f68fe2d2e1a999dfa0
Hu Yin <91766974+hudadaya@users.noreply.github.com>
2024-09-06 15:30:11 +0800

    Update helpers.js

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -109,19 +109,35 @@ export default {
 
     getIceServer() {
         return {
+            // iceServers: [
+            //     {
+            //         urls: ["stun:eu-turn4.xirsys.com"]
+            //     },
+            //     {
+            //         username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
+            //         credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+            //         urls: [
+            //             "turn:eu-turn4.xirsys.com:80?transport=udp",
+            //             "turn:eu-turn4.xirsys.com:3478?transport=tcp"
+            //         ]
+            //     }
+            // ]
             iceServers: [
-                {
-                    urls: ["stun:eu-turn4.xirsys.com"]
-                },
-                {
-                    username: "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-                    credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
-                    urls: [
-                        "turn:eu-turn4.xirsys.com:80?transport=udp",
-                        "turn:eu-turn4.xirsys.com:3478?transport=tcp"
-                    ]
-                }
-            ]
+                {   
+                    urls: [ "stun:ss-turn2.xirsys.com" ]}, 
+                    {   
+                        username: "OnKkFfOcmR2Y3XMFLwKrtNmjw6m_DkcJPkOveEX-ssdHD8k4J5c6FBwguM117CCMAAAAAGa9tmBodWRhZGE=",   
+                        credential: "e49991f4-5adc-11ef-b63a-0242ac140004",   
+                        urls: [       
+                            "turn:ss-turn2.xirsys.com:80?transport=udp",       
+                            "turn:ss-turn2.xirsys.com:3478?transport=udp",       
+                            "turn:ss-turn2.xirsys.com:80?transport=tcp",       
+                            "turn:ss-turn2.xirsys.com:3478?transport=tcp",       
+                            "turns:ss-turn2.xirsys.com:443?transport=tcp",       
+                            "turns:ss-turn2.xirsys.com:5349?transport=tcp"   
+                        ]
+                    }
+                ]
         };
     },
 
